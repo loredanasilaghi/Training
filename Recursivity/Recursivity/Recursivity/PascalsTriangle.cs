0826af49ac427aed8e41e810f6a40d96f909beb1
loredanasilaghi <loredana.silaghi@gmail.com>
2015-10-14 16:49:20 +0300

    refactoring + triangle solution

---
+++ Recursivity/Recursivity/Recursivity/PascalsTriangle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Recursivity
+{
+    [TestClass]
+    public class PascalsTriangle
+    {
+        [TestMethod]
+        public void TestWithRow4()
+        {
+            int rowNumber = 4;
+            int[][] expected = new int[rowNumber][];
+            expected[0] = new int[] { 1 };
+            expected[1] = new int[] { 1, 1 };
+            expected[2] = new int[] { 1, 2, 1 };
+            expected[3] = new int[] { 1, 3, 3, 1 };
+            for (int i = 0; i < rowNumber; i++)
+                CollectionAssert.AreEqual(expected[i], GenerateRow(rowNumber)[i]);
+        }
+
+        [TestMethod]
+        public void TestWithRow0()
+        {
+            int[][] expected = { };
+            CollectionAssert.AreEqual(expected, GenerateRow(0));
+        }
+
+        [TestMethod]
+        public void TestWithRow1()
+        {
+            int rowNumber = 1;
+            int[][] expected = new int[rowNumber] [];
+            expected[0] = new int[] { 1 };
+            for (int i = 0; i<rowNumber; i++)
+                CollectionAssert.AreEqual(expected[i], GenerateRow(rowNumber)[i]);
+        }
+
+        [TestMethod]
+        public void TestWithRow2()
+        {
+            int rowNumber = 2;
+            int[][] expected = new int[2][];
+            expected[0] = new int[] { 1 };
+            expected[1] = new int[] { 1, 1 };
+            for (int i = 0; i < rowNumber; i++)
+                CollectionAssert.AreEqual(expected[i], GenerateRow(rowNumber)[i]);
+        }
+
+        public static int[][] GenerateRow(int rowNumber)
+        {
+            int[][] triangle = new int[rowNumber][];
+            for (int i = 1; i <= rowNumber; i++)
+            {
+                triangle[i-1] = new int[i];
+                for (int j = 0; j < i; j++)
+                {
+                    triangle[i-1][j] = GenerateNumbers(i, j);
+                }
+            }
+            return triangle;
+        }
+        
+        public static int GenerateNumbers(int row, int column)
+        {
+            if (row == 0 || column == 0 || row == column + 1)
+                return 1;
+            int leftNumber = 0;
+            int rightNumber = 0;
+            leftNumber = GenerateNumbers(row - 1, column - 1);
+            rightNumber = GenerateNumbers(row - 1, column);
+            return leftNumber + rightNumber;
+        }
+
+    }
+}
--- Recursivity/Recursivity/Recursivity/ReverseAndReplace.cs
+++ Recursivity/Recursivity/Recursivity/ReverseAndReplace.cs
@@ -7,6 +7,13 @@ namespace Recursivity
     public class ReverseAndReplace
     {
         [TestMethod]
+        public void TestReverseEmptyString()
+        {
+            Assert.AreEqual("", ReverseGivenString(""));
+        }
+        
+
+        [TestMethod]
         public void TestReverseString()
         {
             Assert.AreEqual("jihgfedcba", ReverseGivenString("abcdefghij"));
@@ -25,36 +32,54 @@ namespace Recursivity
         }
 
         [TestMethod]
+        public void TestReplaceCharFromEmptyString()
+        {
+            Assert.AreEqual("", ReplaceChar("", 'e', 'x'));
+        }
+
+        [TestMethod]
+        public void TestReplaceCharNotFromString()
+        {
+            Assert.AreEqual("tea", ReplaceChar("tea", 'i', 'x'));
+        }
+
+        [TestMethod]
         public void TestReplaceCharFromString()
         {
             Assert.AreEqual("massassappa", ReplaceChar("mississippi", 'i', 'a'));
         }
 
+        [TestMethod]
+        public void TestReplaceCharStartingWithFromString()
+        {
+            Assert.AreEqual("anataally", ReplaceChar("initially", 'i', 'a'));
+        }
+
         public static string ReverseGivenString(string givenString)
         {
 
-            if (givenString.Length > 0)
-            {
-                string substring = givenString.Substring(0, givenString.Length - 1);
-                string reversedSubstring = ReverseGivenString(substring);
-                return givenString[givenString.Length - 1] + reversedSubstring;
-            }
-            else
-                return givenString;
+            if (givenString.Length == 0) return givenString;
+            string substring = givenString.Substring(0, givenString.Length - 1);
+            string reversedSubstring = ReverseGivenString(substring);
+            return givenString[givenString.Length - 1] + reversedSubstring;
         }
 
-        public static string ReplaceChar(string word, char toBeReplaced, char replace)
+        public static string ReplaceChar(string word, char toBeReplaced, char replacement)
         {
-            if (word.Length > 0)
-            {
-                if (word[0] == toBeReplaced)
-                    word = word.Replace(word[0], replace);
-                string substring = word.Substring(1, word.Length - 1);
-                string replacedSubstring = ReplaceChar(substring, toBeReplaced, replace);
-                return word[0] + replacedSubstring;
-            }
-            else
-                return word;
+            if (word.Length == 0) return word;
+            char firstChar = word[0];
+            char replacedfirstChar = ReplaceFirstChar(firstChar, toBeReplaced, replacement);
+            string substring = word.Substring(1, word.Length - 1);
+            string replacedSubstring = ReplaceChar(substring, toBeReplaced, replacement);
+            return replacedfirstChar + replacedSubstring;
         }
+
+        public static char ReplaceFirstChar(char firstChar, char toBeReplaced, char replacement)
+        {
+            if (firstChar == toBeReplaced)
+                firstChar = replacement;
+            return firstChar;
+        }
+
     }
 }
