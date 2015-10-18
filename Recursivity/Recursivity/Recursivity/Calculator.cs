fe0ab7cdf911ef925203176fe4b065acde1169e5
loredanasilaghi <loredana.silaghi@gmail.com>
2015-10-19 00:42:38 +0300

    Modified solution for Calculator and added solution for Hanoi Towers

--- Recursivity/Recursivity/Recursivity/Calculator.cs
+++ Recursivity/Recursivity/Recursivity/Calculator.cs
@@ -7,56 +7,127 @@ namespace Recursivity
     public class Calculator
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestInvalidInput()
+        {
+            Assert.AreEqual(-1, CalculateOperation("* + ee 12 15 20"));
+        }
+
+        [TestMethod]
+        public void TestCalculationOfHigherIntegerNumbers()
         {
             Assert.AreEqual(540, CalculateOperation("* + 12 15 20"));
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestSimpleCalculation()
         {
-            Assert.AreEqual(12.6, CalculateOperation("* 3 4.2"));
+            Assert.AreEqual(12.6, CalculateOperation("* 3 4.2"), 0.2);
         }
 
         [TestMethod]
-        public void TestMethod3()
+        public void TestComplexCalculation()
         {
             Assert.AreEqual(1136.25, CalculateOperation("* / * + 56 45 45 3 0.75"));
         }
 
         [TestMethod]
-        public void TestMethod4()
+        public void TestCalculationUsingSubtractAndDivide()
         {
             Assert.AreEqual(-0.5, CalculateOperation("/ - 2 3 2"));
+        }
+
+        [TestMethod]
+        public void TestCalculationOfSpecialCase()
+        {
+            Assert.AreEqual(26, CalculateOperation("+ * + 1 2 + 3 4 5"));
         }
 
         public static double CalculateOperation(string toCalculate)
         {
             char separator = ' ';
             string[] elements = toCalculate.Split(separator);
-            string operatorVar = elements[0];
-            string lastElement = elements[elements.Length - 1].Replace('.', ',');
-            double lastNumber = Convert.ToDouble(lastElement);
-            if (lastElement == operatorVar)
-                return lastNumber;
-            int length = (int)(toCalculate.Length - 2 - operatorVar.Length - lastElement.Length);
-            string substring = toCalculate.Substring(operatorVar.Length + 1, length);
+            int index = FindIndex(elements);
+            if (index == -1)
+                return -1;
 
-            
+            string operatorVar = elements[index];
+            string firstNumberString = elements[index + 1];
+            string secondNumberString = elements[index + 2];
+            double firstNumber = Convert.ToDouble(firstNumberString);
+            double secondNumber = Convert.ToDouble(secondNumberString);
+            double result = DoOperation(operatorVar, firstNumber, secondNumber);
+
+            int lastOperatorIndex = toCalculate.LastIndexOf(elements[index]);
+            string firstSubstring = toCalculate.Substring(0, lastOperatorIndex);
+
+            int startPosition = lastOperatorIndex + 2 + operatorVar.Length + firstNumberString.Length
+                + secondNumberString.Length;// 2 is the number of spaces
+            int length = toCalculate.Length - startPosition;
+            string secondSubstring = toCalculate.Substring(startPosition, length);
+
+            string substring = firstSubstring + result.ToString() + secondSubstring;
+            if (firstSubstring.Length !=0)
+            {
+                result = CalculateOperation(substring);
+            }
+            return result;
+        }
+
+        public static bool IsOperator(string operatorVar)
+        {
+            if ((operatorVar == "+") || (operatorVar == "-") || (operatorVar == "*") || (operatorVar == "/"))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsValidChar(string character)
+        {
+            if (IsOperator(character))
+                return true;
+            try
+            {
+                Convert.ToDouble(character);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+         }
+
+        public static int FindIndex(string[] elements)
+        {
+            int index = 0;
+            for (index = elements.Length - 1; index >= 0; index--)
+            {
+                if (IsOperator(elements[index]))
+                    break;
+                if (!IsValidChar(elements[index]))
+                    return -1;
+            }
+            return index;
+        }
+
+        public static double DoOperation(string operatorVar, double firstNumber, double secondNumber)
+        {
+            double result = 0;
             switch (operatorVar)
             {
-                case  "+":
-                    return Math.Round(CalculateOperation(substring) + lastNumber, 2);
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
                 case "-":
-                    return Math.Round(CalculateOperation(substring) - lastNumber, 2);
+                    result = firstNumber - secondNumber;
+                    break;
                 case "*":
-                    return Math.Round(CalculateOperation(substring) * lastNumber, 2);
+                    result = firstNumber * secondNumber;
+                    break;
                 case "/":
-                    return Math.Round(CalculateOperation(substring) / lastNumber, 2);
-                default:
-                    return lastNumber;
+                    result = firstNumber / secondNumber;
+                    break;
             }
-            
+            return result;
         }
     }
 }
---
+++ Recursivity/Recursivity/Recursivity/HanoiTowers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Recursivity
+{
+    [TestClass]
+    public class HanoiTowers
+    {
+        [TestMethod]
+        public void TestWith3Disks()
+        {
+            int diskNumber = 3;
+            List<int> towerA = new List<int> (PopulateList(diskNumber));
+            List<int> towerB = new List<int>();
+            List<int> towerAux = new List<int>();
+            List<int> expected = new List<int>(towerA);
+            MoveDisks(diskNumber, ref towerA, ref towerAux, ref towerB);
+            CollectionAssert.AreEqual(expected, towerB);
+        }
+
+        [TestMethod]
+        public void TestWith1Disk()
+        {
+            int diskNumber = 1;
+            List<int> towerA = new List<int>(PopulateList(diskNumber));
+            List<int> towerB = new List<int>();
+            List<int> towerAux = new List<int>();
+            List<int> expected = new List<int>(towerA);
+            MoveDisks(diskNumber, ref towerA, ref towerAux, ref towerB);
+            CollectionAssert.AreEqual(expected, towerB);
+        }
+
+        [TestMethod]
+        public void TestWith24Disks()
+        {
+            int diskNumber = 24;
+            List<int> towerA = new List<int>(PopulateList(diskNumber));
+            List<int> towerB = new List<int>();
+            List<int> towerAux = new List<int>();
+            List<int> expected = new List<int>(towerA);
+            MoveDisks(diskNumber, ref towerA, ref towerAux, ref towerB);
+            CollectionAssert.AreEqual(expected, towerB);
+        }
+
+        public static void MoveDisks(int diskNumber, ref List<int> initial, ref List<int> auxiliar, ref List<int> final)
+        {
+            int position = initial.IndexOf(diskNumber);
+
+            if (position == 0)
+            {
+                initial.Remove(diskNumber);
+                final.Insert(0, diskNumber);
+            }
+
+            else
+            {
+                int nextDisk = initial[position - 1];
+                MoveDisks(nextDisk, ref initial, ref final, ref auxiliar);
+                MoveDisks(diskNumber, ref initial, ref auxiliar, ref final);
+                MoveDisks(nextDisk, ref auxiliar, ref initial, ref final);
+            }
+        }
+
+        public static List<int> PopulateList(int numberEntries)
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i <= numberEntries; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+    }
+}
