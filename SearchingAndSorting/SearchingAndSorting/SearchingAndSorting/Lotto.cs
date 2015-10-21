256be2f9adafd60fc235b8240971edd34d45778b
loredanasilaghi <loredana.silaghi@gmail.com>
2015-10-21 17:30:03 +0300

    Added Solution for sorting - first two problems

---
+++ SearchingAndSorting/SearchingAndSorting/SearchingAndSorting/Lotto.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SearchingAndSorting
+{
+    [TestClass]
+    public class Lotto
+    {
+        [TestMethod]
+        public void TestSortedLottoNumbers()
+        {
+            int[] expected = GenerateLottoExtractions(6);
+            QuickSort(expected, 0, 5);
+            Assert.IsTrue(IsSorted(expected));
+        }
+
+        public static bool IsSorted(int[] numbers)
+        {
+            bool isSorted = false;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                isSorted = false;
+                if (numbers[i] < numbers[i + 1])
+                    isSorted = true;
+            }
+            return isSorted;
+        }
+
+        public static int[] GenerateLottoExtractions(int numberExtractions)
+        {
+            List<int> entireList = PopulateList(49);
+            var random = new Random();
+            var result = entireList.OrderBy(item => random.Next()).ToArray();
+            int[] resultArray = result.ToArray();
+            Array.Resize(ref resultArray, 6);
+            return resultArray;
+        }
+
+        public static List<int> PopulateList(int numberEntries)
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i <= numberEntries; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(array, left, right);
+
+                if (pivot > 1)
+                    QuickSort(array, left, pivot - 1);
+
+                if (pivot + 1 < right)
+                    QuickSort(array, pivot + 1, right);
+            }
+        }
+
+        public static int Partition(int[] numbers, int left, int right)
+        {
+            int pivot = numbers[left];
+            while (true)
+            {
+                while (numbers[left] < pivot)
+                    left++;
+
+                while (numbers[right] > pivot)
+                    right--;
+
+                if (left < right)
+                {
+                    Swap(numbers, left, right);
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        public static void Swap(int[] numbers, int left, int right)
+        {
+            int temp = numbers[right];
+            numbers[right] = numbers[left];
+            numbers[left] = temp;
+        }
+    }
+}
---
+++ SearchingAndSorting/SearchingAndSorting/SearchingAndSorting/RepairShop.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SearchingAndSorting
+{
+    [TestClass]
+    public class RepairShop
+    {
+        [TestMethod]
+        public void TestRandomCasesStartingWith1()
+        {
+            int[] initial = { 1, 3, 2, 1, 3, 2 };
+            int[] expected = { 1, 1, 2, 2, 3, 3};
+            CollectionAssert.AreEquivalent(expected, QuickSort3(initial, 0, 5));
+        }
+
+        [TestMethod]
+        public void TestRandomCasesStartingWith2()
+        {
+            int[] initial = { 2, 3, 2, 1, 3, 3, 2, 1, 1, 2 };
+            int[] expected = { 1, 1, 1, 2, 2, 2, 2, 3, 3, 3 };
+            CollectionAssert.AreEquivalent(expected, QuickSort3(initial, 0, 9));
+        }
+
+        [TestMethod]
+        public void TestWithoutMediumValue()
+        {
+            int[] initial = { 3, 1, 3, 1 };
+            int[] expected = { 1, 1, 3, 3 };
+            CollectionAssert.AreEquivalent(expected, QuickSort3(initial, 0, 3));
+        }
+        
+        [TestMethod]
+        public void TestRandomCasesStartingWith3()
+        {
+            int[] initial = { 3, 1, 2 };
+            int[] expected = { 1, 2, 3 };
+            CollectionAssert.AreEquivalent(expected, QuickSort3(initial, 0, 2));
+        }
+
+        [TestMethod]
+        public void TestWith1Case()
+        {
+            int[] initial = { 3 };
+            int[] expected = { 3 };
+            CollectionAssert.AreEquivalent(expected, QuickSort3(initial, 0, 0));
+        }
+
+        [TestMethod]
+        public void TestWith2Cases()
+        {
+            int[] initial = { 3, 2 };
+            int[] expected = { 2, 3 };
+            CollectionAssert.AreEquivalent(expected, QuickSort3(initial, 0, 1));
+        }
+
+        public static int[] QuickSort3(int[] numbers, int first, int last)
+        {
+            int pivotValue = 2;
+            int lessThan = first;
+            int greaterThan = last;
+            int element = first;
+            while (element <= greaterThan)
+            {
+                if (numbers[element] < pivotValue)
+                {
+                    lessThan++;
+                    element++;
+                }
+                if (numbers[element] > pivotValue)
+                {
+                    Swap(numbers, greaterThan, element);
+                    greaterThan--;
+                }
+                if (numbers[element] == pivotValue)
+                    element++;
+            }
+            return numbers;
+        }
+
+        public static void Swap(int[] numbers, int left, int right)
+        {
+            int temp = numbers[right];
+            numbers[right] = numbers[left];
+            numbers[left] = temp;
+        }
+    }
+}
